4d603203e43a2596849d667b52c211ee6fb7dd13
Sayoni <sayonidey.official@gmail.com>
2026-03-26 11:49:19 +0530

    fixed cors issue

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000", // Your Next.js URL
+  origin: "*", // Your Next.js URL
   credentials: true 
 }));
 app.use(express.json());
--- frontend/lib/api.jsx
+++ frontend/lib/api.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://charity-subscription.onrender.com",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://charity-subscription.onrender.com/api",
 });
 
 // This interceptor ensures the token is fresh on every request
